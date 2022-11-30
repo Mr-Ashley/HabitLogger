8ee03e0f7cce5ee87f1b8c355f4bf89de456705c
Ashley Abrahams <Ashley.Abrahams@1Nebula.com>
2022-11-30 10:14:49 +0200

    Add connection to database

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using Microsoft.Data.Sqlite;
+
+string connectionString = @"Data Source=habitLogger.db";
+
+using (var connection = new SqliteConnection(connectionString))
+{
+    connection.Open();
+
+    var tableCmd = connection.CreateCommand();
+
+    tableCmd.CommandText = @"CREATE TABLE IF NOT EXISTS drink_water(
+                            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            Date TEXT,
+                            Quantity INTEGER)";
+    tableCmd.ExecuteNonQuery();
+
+    connection.Close();
+}
